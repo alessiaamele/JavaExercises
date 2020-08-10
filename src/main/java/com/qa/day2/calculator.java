4d2ca930527a1c4fab4266bc136a83c4c606bc1a
alessia M <alessia.mele.097@gmail.com>
2020-08-10 15:59:54 +0100

    Method 1 draft: sum

---
+++ src/main/java/com/qa/day2/calculator.java
@@ -0,0 +1,10 @@
+package com.qa.day2;
+
+public class calculator {
+
+    public int Method1(int integer1, int integer2){
+        int result = integer1 + integer2
+                return result
+    }
+
+}
