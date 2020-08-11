92294fdd5d87479483a3c90f252515ea1d21fed5
alessia M <alessia.mele.097@gmail.com>
2020-08-11 11:49:58 +0100

    Created the first 2 flowcharts

---
+++ src/main/java/com/qa/day3/Flowcharts.java
@@ -0,0 +1,29 @@
+package com.qa.day3;
+
+public class Flowcharts {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int diagram1() {
+        for (int A = 100; A <= 200; A++) {
+            System.out.println("A");
+        }
+    }
+
+    public static int diagram1(){
+        for (int A=100; A<=200; A++){
+            if (A%2==0){
+                System.out.println("-");
+            }
+            else{
+                System.out.println("*");
+            }
+        }
+    }
+
+    public static int method3(){
+        for()
+    }
+
