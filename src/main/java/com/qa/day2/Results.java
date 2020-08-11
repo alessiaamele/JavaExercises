69eacb8e3cc0fc0dc259a5b7fb5f37c431d1bc06
alessia M <alessia.mele.097@gmail.com>
2020-08-11 11:02:22 +0100

    updated "Results" with Method 1 and 2

--- src/main/java/com/qa/day2/Results.java
+++ src/main/java/com/qa/day2/Results.java
@@ -8,6 +8,10 @@ public class Results {
     }
 
     public static int Method1(int chemGrade, int phyGrade, int bioGrade){
+        if (chemGrade/150 <60 || phyGrade/150 <60 || bioGrade/150<60){
+            System.out.println( "You failed");
+            return 0;
+        }
         System.out.println("Chemistry grade:"+ chemGrade);
         System.out.println("Physics grade:"+ phyGrade);
         System.out.println("Biology grade:"+ bioGrade);
@@ -18,8 +22,25 @@ public class Results {
 
     public static int Method2(int chemGrade, int phyGrade, int bioGrade){
         int totalGrade = chemGrade + phyGrade + bioGrade;
+        if (chemGrade<60 || phyGrade<60 || bioGrade<60){
+            System.out.println("You failed");
+            return 0;
+        }
+        int overallGrade = totalGrade*100/450;
+        if (overallGrade>60) {
+            System.out.println("Overall grade:");
+            return overallGrade;
+        }
+        else{
+            System.out.println("You failed")
+        }
+    }
+
+    public static int Method2(int chemGrade, int phyGrade, int bioGrade){
+        int totalGrade = chemGrade + phyGrade + bioGrade;
         int overallGrade = totalGrade*100/450;
         System.out.println("Overall grade:");
         return overallGrade;
     }
+
 }
