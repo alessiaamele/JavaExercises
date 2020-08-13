1d031c6f2bb0e2c1c79d282ef52d39a3e90e83c6
alessia M <alessia.mele.097@gmail.com>
2020-08-13 15:44:07 +0100

    Peer programming

--- src/main/java/com/qa/day2/Calculator.java
+++ src/main/java/com/qa/day2/Calculator.java
@@ -5,30 +5,30 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-        System.out.println(Method1day2bis(2,4));
-        System.out.println(Method2day2bis(2,4));
-        System.out.println(Method3day2bis(2,4));
-        System.out.println(Method4day2bis(2,4));
+        System.out.println(Sum(2,4));
+        System.out.println(Multiplication(2,4));
+        System.out.println(Subtraction(2,4));
+        System.out.println(Division(2,4));
 
     }
 
-    public static int Method1day2bis(int integer1, int integer2){
+    public static int Sum(int integer1, int integer2){
         int result = integer1 + integer2;
                 return result;
     }
 
-    public static int Method2day2bis(int integer1, int integer2){
+    public static int Multiplication(int integer1, int integer2){
         int result = integer1 * integer2;
         return result;
     }
 
 
-    public static int Method3day2bis(int integer1, int integer2){
+    public static int Subtraction(int integer1, int integer2){
         int result = integer1 - integer2;
         return result;
     }
 
-    public static double Method4day2bis(double integer1, double integer2){
+    public static double Division(double integer1, double integer2){
         if (integer1>integer2){
             double result = integer1 / integer2;
             System.out.println(result);
--- src/test/java/com/qa/AppTest.java
+++ src/test/java/com/qa/AppTest.java
@@ -1,21 +1,27 @@
 package com.qa;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
+    //@Before
+    //Calculator calculator = new Calculator();
 
-        assertTrue( true );
+    @Test
+    public void test1() {
+        Calculator calculator = new Calculator();
+        assertEquals( "2+3 should equal 5",5,calculator.Sum(2,3) );
+        assertEquals( "2*3 should equal 6",6,calculator.Multiplication(2,3) );
+        assertEquals( "2-3 should equal -1",-1,calculator.Substraction(2,3) );
+        assertEquals( "6/3 should equal 2",2,calculator.Division(6,3) );
     }
+
+   // @After
+   // calculator = null;
 }
